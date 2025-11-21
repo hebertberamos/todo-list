eb41c49862ddc6fb8d4a55a59062f8475996cc39
hebertberamos <hebertramos09@gmail.com>
2025-11-21 00:35:02 -0300

    First commit to start the project

---
+++ backend/src/main/java/com/personal/todolist/entitites/Task.java
@@ -0,0 +1,26 @@
+package com.personal.todolist.entitites;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_tasks")
+@Getter
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Task() {}
+}
